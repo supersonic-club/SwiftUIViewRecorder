5d48efed584380819b7fc539ba5fc5f429762fed
Puneet Goyal <puneet@zeta.tech>
2022-03-03 20:13:51 +0530

    add a public init method

--- Sources/SwiftUIViewRecorder/Video/VideoRenderer.swift
+++ Sources/SwiftUIViewRecorder/Video/VideoRenderer.swift
@@ -3,6 +3,7 @@ import UIImageExtensions
 import Combine
 
 public class VideoRenderer: FramesRenderer {
+    public init() {}
     public func render(frames: [UIImage], framesPerSecond: Double) -> Future<URL?, Error> {
         frames.toVideo(framesPerSecond: framesPerSecond)
     }
